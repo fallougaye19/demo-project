b0185a3ccc83114f481a446bd732fb8475d164d8
fallou <fallougaye303@gmail.com>
2026-03-07 23:26:32 +0000

    Mise a jour du projet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const AWS_SECRET = "AKIAIOSFODNN7EXAMPLE";
 
 app.get('/user', (req, res) => {
     res.send(req.query.name); // vulnérable XSS
